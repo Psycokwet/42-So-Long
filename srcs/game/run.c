8aee7728e8b46f870bd5333cbb96287e56bfbbb3
Sophie <socarboni@gmail.com>
2021-09-24 14:25:23 +0200

    print action number on term

--- srcs/game/run.c
+++ srcs/game/run.c
@@ -6,7 +6,7 @@
 /*   By: scarboni <scarboni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/22 18:54:29 by scarboni          #+#    #+#             */
-/*   Updated: 2021/09/24 14:17:37 by scarboni         ###   ########.fr       */
+/*   Updated: 2021/09/24 14:24:41 by scarboni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,17 +25,15 @@ int should_update_axe(char map_case, char reject_move, int *current_axe, int *ne
 
 static int	actually_run(t_env *env)
 {
-	int	did_move;
-
-	did_move = false;
-	printf("run\n");
+	// printf("run\n");
 	t_coordinates new_pos = (t_coordinates){env->current_pos.x + env->try_to_run_dir.x * RUN_STEP, env->current_pos.y + env->try_to_run_dir.y * RUN_STEP};
 
-	did_move = should_update_axe(env->map_array.lines[new_pos.y][env->current_pos.x], AUTHORIZED_ON_MAP_WALL, &env->current_pos.y, &new_pos.y) 
-	| should_update_axe(env->map_array.lines[(int)(env->current_pos.y)][(int)new_pos.x], AUTHORIZED_ON_MAP_WALL, &env->current_pos.x, &new_pos.x);
-	if (did_move == true)
+	if ((should_update_axe(env->map_array.lines[new_pos.y][env->current_pos.x], AUTHORIZED_ON_MAP_WALL, &env->current_pos.y, &new_pos.y) 
+	| should_update_axe(env->map_array.lines[(int)(env->current_pos.y)][(int)new_pos.x], AUTHORIZED_ON_MAP_WALL, &env->current_pos.x, &new_pos.x)) == true)
 		env->count++;
-	printf("RUNNING NEW POS %d:%d::%d\n", env->current_pos.x, env->current_pos.y, env->count);
+	// printf("RUNNING NEW POS %d:%d::%d\n", env->current_pos.x, env->current_pos.y, env->count);
+	ft_putstr_fd("\r", 1);
+	ft_putnbr_fd(env->count, 1);
 	env->try_to_run_dir = (t_coordinates){0, 0};
 	return (EXIT_SUCCESS);
 }
